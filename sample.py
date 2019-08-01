7773852ee9bcfa6ba4d0050bab6cf51543eb5661
s8sg <swarvanusg@gmail.com>
2019-08-01 20:31:16 +0900

    Add example
    Signed-off-by: s8sg <swarvanusg@gmail.com>

--- sample.py
+++ sample.py
@@ -1,8 +1,3 @@
-import time
 def run(params=None):
     string = "Sample python script run!"
-    for i in range(10):
-        string = string + " iteration"
-        time.sleep(6)
-
     return string
