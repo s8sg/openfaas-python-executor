62730e2032321cf1d0a09fc4de8fb27bdbafe960
s8sg <swarvanusg@gmail.com>
2019-08-01 12:05:11 +0900

    Add sample script
    Signed-off-by: s8sg <swarvanusg@gmail.com>

--- sample.py
+++ sample.py
@@ -1,2 +1,2 @@
-def run():
+def run(params=None):
     return "Sample python script run!"
--- test/handler.py
+++ test/handler.py
@@ -1,7 +1,7 @@
 import json
 import os
-import importlib
 import os.path
+import sys
 
 def handle(data):
     res = ""
@@ -13,23 +13,19 @@ def handle(data):
     except Exception:
         res = "failed yo load json"
         return res
-    job_id = req["job-id"]
+    job_id = req.get("job-id", "")
     if job_id == "":
         res = "job-id not provided"
         return res
-    git_url = req["git-url"]
+    git_url = req.get("git-url", "")
     if git_url == "":
         res = "git-url not provided"
         return res
-    script_name = req["script"]
+    script_name = req.get("script", "")
     if script_name == "":
         res = "script not provided"
         return res
-    params = req["params"]
-    if params == "":
-        res = "params not provided"
-        return res
-
+    params = req.get("params", "")
 
     commands = []
 
@@ -38,23 +34,25 @@ def handle(data):
     clone_command = 'git clone ' + git_url + ' /tmp/' + job_id + '/git'
     commands.append(clone_command)
 
+    #touch_command = 'touch ./'+ job_id + '/git/__init__.py;' + 'touch ./'+ job_id + '/__init__.py'
+    #commands.append(touch_command)
 
-    for index in commands:
-        os.system(commands[index])
+    for command in commands:
+        os.system(command)
     
     if os.path.isfile('/tmp/' + job_id + '/git/requirements.txt'):
         install_requirments = 'pip install -r ' + '/tmp/' + job_id + '/git/requirements.txt' 
         os.system(install_requirments)
 
-    importlocation = '/tmp/' + job_id + '/git/' + script_name
 
-    script_module = None
+    importlocation = '/tmp/' + job_id + '/git'
+
+    sys.path.append(importlocation)
     try:
-        script_module = importlib.import_module(importlocation)
-    except Exception:
-        res = "failed to import script"
-        return res
-
-    res = script_module.run()
+        from sample import run 
+        res = run(params=params)
+    except Exception as e:
+        res = "failed to import script: " + str(e)
+    sys.path.remove(importlocation)
 
     return res
